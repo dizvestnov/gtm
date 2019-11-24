3bc33ad350d1e40976ca0527b5101c46cd741c16
dizvestnov <ivestarator@gmail.com>
2019-11-24 22:59:12 +0700

    Fixed docker-compose.yml, instell AdminLTE theme, edited common params.php, remove Vagrantfile, add vscode settings file to .gitignore

--- common/config/params.php
+++ common/config/params.php
@@ -1,8 +1,8 @@
 <?php
 return [
-    'adminEmail' => 'admin@example.com',
-    'supportEmail' => 'support@example.com',
-    'senderEmail' => 'noreply@example.com',
-    'senderName' => 'Example.com mailer',
-    'user.passwordResetTokenExpire' => 3600,
+	'adminEmail' => 'ivestarator@example.com',
+	'supportEmail' => 'ivestarator@example.com',
+	'senderEmail' => 'noreply@example.com',
+	'senderName' => 'Example.com mailer',
+	'user.passwordResetTokenExpire' => 3600,
 ];
