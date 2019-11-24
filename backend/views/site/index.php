8064e4762cdf57b530c7a255aa06d77ea0152368
dizvestnov <ivestarator@gmail.com>
2019-11-24 23:04:59 +0700

    Connected and configured the theme for backend, 3 files in doubt - search, controller, ALPAsset

--- backend/views/site/index.php
+++ backend/views/site/index.php
@@ -1,53 +1,130 @@
 <?php
-
 /* @var $this yii\web\View */
-
 $this->title = 'My Yii Application';
 ?>
 <div class="site-index">
+	<div class="body-content">
+		<!-- Direct Chat -->
+		<!-- <section id="direct-chat">
+			<div class="col-xs-5">
+				<div class="box box-warning direct-chat direct-chat-warning">
+					<div class="box-header with-border">
+						<h3 class="box-title">
+							<font style="vertical-align: inherit;">
+								<font style="vertical-align: inherit;">Прямой чат</font>
+							</font>
+						</h3>
+						<span data-toggle="tooltip" title="3 новых сообщения" class="badge label label-warning">
+							<font style="vertical-align: inherit;">
+								<font style="vertical-align: inherit;">3</font>
+							</font>
+						</span>
+						<div class="box-tools pull-right">
+							<button class="btn btn-box-tool" data-toggle="tooltip" title="контакты" data-widget="chat-pane-toggle">
+								<i class="fa fa-user-circle-o"></i>
+							</button>
+							<button class="btn btn-box-tool" data-widget="collapse">
+								<i class="fa fa-minus"></i>
+							</button>
+							<button class="btn btn-box-tool" data-widget="remove">
+								<i class="fa fa-times"></i>
+							</button>
+						</div>
+					</div>
+					<div class="box-body">
+						<div class="direct-chat-messages">
+							<div class="direct-chat-msg">
+								<div class="direct-chat-info clearfix">
+									<span class="direct-chat-name pull-left">
+										<font style="vertical-align: inherit;">
+											<font style="vertical-align: inherit;">Александр Пирс </font>
+										</font>
+									</span>
+									<span class="direct-chat-timestamp pull-right">
+										<font style="vertical-align: inherit;">
+											<font style="vertical-align: inherit;">23 января 14:00</font>
+										</font>
+									</span>
+								</div>
+								<img class="direct-chat-img" src="/assets/425aa529/img/user2-160x160.jpg" alt="сообщение пользователя">
+								<div class="direct-chat-text">
+									<font style="vertical-align: inherit;">
+										<font style="vertical-align: inherit;">
+											Этот шаблон действительно бесплатно? </font>
+										<font style="vertical-align: inherit;">Это невероятно!
+										</font>
+									</font>
+								</div>
+							</div>
+							<div class="direct-chat-msg right">
+								<div class="direct-chat-info clearfix">
+									<span class="direct-chat-name pull-right">
+										<font style="vertical-align: inherit;">
+											<font style="vertical-align: inherit;">Сара Баллок </font>
+										</font>
+									</span>
+									<span class="direct-chat-timestamp pull-left">
+										<font style="vertical-align: inherit;">
+											<font style="vertical-align: inherit;">23 января 14:05</font>
+										</font>
+									</span>
+								</div>
+								<img class="direct-chat-img" src="/assets/425aa529/img/user3-128x128.jpg" alt="сообщение пользователя">
+								<div class="direct-chat-text">
+									<font style="vertical-align: inherit;">
+										<font style="vertical-align: inherit;">
+											Тебе лучше поверить в это!
+										</font>
+									</font>
+								</div>
+							</div>
+						</div>
+						<div class="direct-chat-contacts">
+							<ul class="contacts-list">
+								<li>
+									<a href="#">
+										<img class="contacts-list-img" src="/assets/425aa529/img/user4-128x128.jpg" alt="Контакт Аватар">
+										<div class="contacts-list-info">
+											<span class="contacts-list-name">
+												<font style="vertical-align: inherit;">
+													<font style="vertical-align: inherit;">
+														Кристина Бокова
+													</font>
+												</font><small class="contacts-list-date pull-right">
+													<font style="vertical-align: inherit;">
+														<font style="vertical-align: inherit;">28.02.2015</font>
+													</font>
+												</small>
+											</span>
+											<span class="contacts-list-msg">
+												<font style="vertical-align: inherit;">
+													<font style="vertical-align: inherit;"> Как дела? </font>
+													<font style="vertical-align: inherit;">Я был...</font>
+												</font>
+											</span>
+										</div>
+									</a>
+								</li>
+							</ul>
+						</div>
+					</div>
+					<div class="box-footer">
+						<form action="#" method="post">
+							<div class="input-group">
+								<input type="text" name="message" placeholder="Type Message ..." class="form-control">
+								<span class="input-group-btn">
+									<button type="button" class="btn btn-warning btn-flat">
+										<font style="vertical-align: inherit;">
+											<font style="vertical-align: inherit;">послать</font>
+										</font>
+									</button>
+								</span>
+							</div>
+						</form>
+					</div>
+				</div>
+			</div>
+		</section> -->
 
-    <div class="jumbotron">
-        <h1>Congratulations!</h1>
-
-        <p class="lead">You have successfully created your Yii-powered application.</p>
-
-        <p><a class="btn btn-lg btn-success" href="http://www.yiiframework.com">Get started with Yii</a></p>
-    </div>
-
-    <div class="body-content">
-
-        <div class="row">
-            <div class="col-lg-4">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/doc/">Yii Documentation &raquo;</a></p>
-            </div>
-            <div class="col-lg-4">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/forum/">Yii Forum &raquo;</a></p>
-            </div>
-            <div class="col-lg-4">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/extensions/">Yii Extensions &raquo;</a></p>
-            </div>
-        </div>
-
-    </div>
+	</div>
 </div>
