b773ff6d63c7d0f49578eb41b8bbbae7c9c78307
dizvestnov <ivestarator@gmail.com>
2019-11-29 18:07:55 +0700

    Final changes for backend adminLTE themes

--- backend/themes/adminlte/layouts/header.php
+++ backend/themes/adminlte/layouts/header.php
@@ -8,7 +8,7 @@ use yii\helpers\Html;
 
 <header class="main-header">
 
-	<?= Html::a('<span class="logo-mini">APP</span><span class="logo-lg">' . Yii::$app->name . '</span>', Yii::$app->homeUrl, ['class' => 'logo']) ?>
+	<?= Html::a('<span class="logo-mini">GTM</span><span class="logo-lg">' . Yii::$app->name . '</span>', Yii::$app->homeUrl, ['class' => 'logo']) ?>
 
 	<nav class="navbar navbar-static-top" role="navigation">
 		<a href="#" class="sidebar-toggle" data-toggle="push-menu" role="button">
